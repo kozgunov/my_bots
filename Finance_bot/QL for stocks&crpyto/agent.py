c279b20e58d044876ec688f04c090c5c9f860a95
Nikita Kozgunovn <78970376+kozgunov@users.noreply.github.com>
2024-10-21 12:41:36 +0300

    Update agent.py

--- Finance_bot/QL for stocks&crpyto/agent.py
+++ Finance_bot/QL for stocks&crpyto/agent.py
@@ -1,15 +1,44 @@
 import numpy as np
 import random
 from collections import deque
-import tensorflow as tf
-from tensorflow.keras.regularizers import l2
-from  tensorflow import keras
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-print(f"TensorFlow version: {tf.__version__}")
-print(f"Keras version: {tf.keras.__version__}")
+class SimplerDQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SimplerDQN, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, output_dim)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
 
 class ImprovedQLearningAgent:
+    """
+    An improved Q-learning agent using a deep neural network (Deep Q-Network).
+    """
+
     def __init__(self, state_size, action_size, learning_rate=0.001, gamma=0.95, epsilon=1.0, epsilon_decay=0.995, epsilon_min=0.01, memory_size=2000, batch_size=32):
+        """
+        Initialize the Deep Q-Network agent.
+
+        Args:
+        state_size (int): Dimension of the state space
+        action_size (int): Number of possible actions
+        learning_rate (float): Learning rate for the optimizer
+        gamma (float): Discount factor for future rewards
+        epsilon (float): Initial exploration rate
+        epsilon_decay (float): Decay rate for epsilon
+        epsilon_min (float): Minimum value for epsilon
+        memory_size (int): Size of the replay memory
+        batch_size (int): Number of samples to use for training
+        """
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=memory_size)
@@ -19,65 +48,125 @@ class ImprovedQLearningAgent:
         self.epsilon_min = epsilon_min
         self.learning_rate = learning_rate
         self.batch_size = batch_size
-        self.delta = 0.01  # For delta-epsilon method
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = SimplerDQN(state_size, action_size).to(self.device)
+        self.target_model = SimplerDQN(state_size, action_size).to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.criterion = nn.MSELoss()
 
-        self.model = self._build_model()
-        self.target_model = self._build_model()
-        self.update_target_model()
-
-    def _build_model(self):
-        model = tf.keras.Sequential([
-            tf.keras.layers.Dense(32, input_dim=self.state_size, activation='relu', kernel_regularizer=l2(0.01)),
-            tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=l2(0.01)),
-            tf.keras.layers.Dense(self.action_size, activation='linear', kernel_regularizer=l2(0.01))
-        ])
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate))
-        return model
+        # New attributes for tracking performance
+        self.wins = 0
+        self.losses = 0
+        self.total_trades = 0
+        self.cumulative_reward = 0
+        self.win_streak = 0
+        self.max_win_streak = 0
 
     def update_target_model(self):
-        self.target_model.set_weights(self.model.get_weights())
+        self.target_model.load_state_dict(self.model.state_dict())
 
     def remember(self, state, action, reward, next_state, done):
+        """
+        Store a transition in the replay memory.
+
+        Args:
+        state: Current state
+        action: Action taken
+        reward: Reward received
+        next_state: Next state
+        done: Whether the episode is done
+        """
         self.memory.append((state, action, reward, next_state, done))
+        
+        # Update performance metrics
+        self.total_trades += 1
+        self.cumulative_reward += reward
+        
+        if reward > 0:
+            self.wins += 1
+            self.win_streak += 1
+            self.max_win_streak = max(self.max_win_streak, self.win_streak)
+        elif reward < 0:
+            self.losses += 1
+            self.win_streak = 0
 
     def get_action(self, state):
+        """
+        Choose an action using an epsilon-greedy policy.
+
+        Args:
+        state: Current state
+
+        Returns:
+        int: Chosen action
+        """
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        q_values = self.model.predict(state, verbose=0)
-        return np.argmax(q_values[0])
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            q_values = self.model(state)
+        return q_values.argmax().item()
 
-    def replay(self):
-        if len(self.memory) < self.batch_size:
+    def replay(self, batch_size):
+        """Train the network on a batch of transitions from the replay memory."""
+        if len(self.memory) < batch_size:
             return
+        minibatch = random.sample(self.memory, batch_size)
+        states, actions, rewards, next_states, dones = zip(*minibatch)
+        
+        states = np.array(states)
+        actions = np.array(actions)
+        rewards = np.array(rewards)
+        next_states = np.array(next_states)
+        dones = np.array(dones)
 
-        minibatch = random.sample(self.memory, self.batch_size)
-        states = np.array([i[0] for i in minibatch])
-        actions = np.array([i[1] for i in minibatch])
-        rewards = np.array([i[2] for i in minibatch])
-        next_states = np.array([i[3] for i in minibatch])
-        dones = np.array([i[4] for i in minibatch])
+        states = torch.FloatTensor(states).to(self.device)
+        actions = torch.LongTensor(actions).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(next_states).to(self.device)
+        dones = torch.FloatTensor(dones).to(self.device)
 
-        states = np.squeeze(states)
-        next_states = np.squeeze(next_states)
+        current_q_values = self.model(states).gather(1, actions.unsqueeze(1))
+        next_q_values = self.target_model(next_states).max(1)[0].detach()
+        target_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
 
-        targets = rewards + self.gamma * (np.amax(self.target_model.predict_on_batch(next_states), axis=1)) * (1 - dones)
-        targets_full = self.model.predict_on_batch(states)
+        loss = self.criterion(current_q_values, target_q_values.unsqueeze(1))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-        ind = np.array([i for i in range(self.batch_size)])
-        targets_full[[ind], [actions]] = targets
-
-        self.model.fit(states, targets_full, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-    def delta_epsilon_update(self, reward):
-        if reward > 0:
-            self.epsilon = max(self.epsilon_min, self.epsilon - self.delta)
-        else:
-            self.epsilon = min(1.0, self.epsilon + self.delta)
+        return loss.item()
 
     def load(self, name):
-        self.model.load_weights(name)
+        """Load model weights from a file."""
+        self.model.load_state_dict(torch.load(name))
+        self.model.eval()
 
     def save(self, name):
-        self.model.save_weights(name)
+        """Save model weights to a file."""
+        torch.save(self.model.state_dict(), name)
+
+    def get_performance_metrics(self):
+        win_rate = self.wins / self.total_trades if self.total_trades > 0 else 0
+        avg_reward = self.cumulative_reward / self.total_trades if self.total_trades > 0 else 0
+        return {
+            'win_rate': win_rate,
+            'total_trades': self.total_trades,
+            'wins': self.wins,
+            'losses': self.losses,
+            'avg_reward': avg_reward,
+            'max_win_streak': self.max_win_streak,
+            'current_win_streak': self.win_streak
+        }
+
+    def reset_performance_metrics(self):
+        self.wins = 0
+        self.losses = 0
+        self.total_trades = 0
+        self.cumulative_reward = 0
+        self.win_streak = 0
+        self.max_win_streak = 0
